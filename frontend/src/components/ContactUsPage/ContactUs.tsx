eda0370826f816f9548917a2b3fbe2a47008666a
tabacalera-osf <juliobandeira2025@gmail.com>
2023-11-21 13:32:14 -0300

    Creating contactUs page with Info and Form Components

--- frontend/src/components/ContactUsPage/ContactUs.tsx
+++ frontend/src/components/ContactUsPage/ContactUs.tsx
@@ -1,8 +1,11 @@
 import styles from "./ContactUs.module.css";
 import { IoIosArrowForward } from "react-icons/io";
+import Form from "./Form";
+import Info from "./Info";
 
 export const ContactUs = () => {
   return (
+    
     <section className={styles["section-container"]}>
       <div className={styles["header-container"]}>
         <div className={styles["header-flex"]}>
@@ -13,6 +16,18 @@ export const ContactUs = () => {
             <span>Contato</span>
           </p>
         </div>
+      </div>
+      <div className={styles["contactus-grid"]}>
+        <h2>Entre em contato conosco</h2>
+          <p className="description-section-title">
+              Faça parte da mudança que você deseja ver no mundo. Encontre seu projeto
+              de voluntariado ideal agora
+          </p>
+      </div>
+      <div className={styles['form-info-container']}>
+        <Info />
+        <Form />
+
       </div>
     </section>
   );
